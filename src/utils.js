c7b38240af1a061d7691f76e456cb056d38d7b3c
Azizi Yazit <ibnuyazit@gmail.com>
2019-12-10 14:10:47 +0800

    Initial commit

---
+++ src/utils.js
@@ -0,0 +1,38 @@
+const items = [
+  "Neptunium",
+  "Plutonium",
+  "Americium",
+  "Curium",
+  "Berkelium",
+  "Californium",
+  "Einsteinium",
+  "Fermium",
+  "Mendelevium",
+  "Nobelium",
+  "Lawrencium",
+  "Rutherfordium",
+  "Dubnium",
+  "Seaborgium",
+  "Bohrium",
+  "Hassium",
+  "Meitnerium",
+  "Darmstadtium",
+  "Roentgenium",
+  "Copernicium",
+  "Nihonium",
+  "Flerovium",
+  "Moscovium",
+  "Livermorium",
+  "Tennessine",
+  "Oganesson"
+];
+const menuStyles = {
+  maxHeight: "200px",
+  overflowY: "auto",
+  width: "150px",
+  margin: 0,
+  borderTop: 0,
+  background: "white"
+};
+
+export { menuStyles, items };
